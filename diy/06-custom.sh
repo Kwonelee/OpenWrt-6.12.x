d0fa23375d946d8bb44ca0d6f5bfea3e0eea00eb
Kwonelee <995546054@qq.com>
2025-11-24 19:40:07 +0800

    Update 06-custom.sh

--- diy/06-custom.sh
+++ diy/06-custom.sh
@@ -44,5 +44,5 @@ cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3566-station-m2-u-boot.dtsi
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/station-m2-rk3566_defconfig package/boot/uboot-rockchip/src/configs/
 
 # 复制dts到files/arch/arm64/boot/dts/rockchip
-mkdir -p target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
+mkdir -p target/linux/rockchip/files/arch/arm64/boot/dts/rockchip
 cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3566-roc-pc.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
